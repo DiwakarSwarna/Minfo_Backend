bc695a5da1f1b86646200c0cb8e57b62008b9162
DiwakarSwarna <157515512+DiwakarSwarna@users.noreply.github.com>
2025-10-15 18:59:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use("/api/farmer", farmerRoutes);
 const PORT = process.env.PORT || 5000;
 // DB + Server
 mongoose
-  .connect("mongodb+srv://diwakarswarna11_db_user:diwakar11@1@cluster0.bmajsrr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .connect("mongodb+srv://diwakarswarna11_db_user:diwakar11@cluster0.bmajsrr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     app.listen(PORT, "0.0.0.0", () =>
       // console.log("Server running on http://localhost:5000", PORT)
